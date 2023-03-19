78fefa8e87410f28823b448e5eabb3e2c01862d1
DESKTOP-1RC8MT9\NghiaLe <nghia.vlm.62cntt@ntu.edu.vn>
2023-03-19 21:54:26 +0700

    Update - Sửa nội dung, thêm sơ đồ ERD

--- ChaoMinBeauty.sql
+++ ChaoMinBeauty.sql
@@ -2,27 +2,17 @@ CREATE DATABASE ChaoMinBeauty
 GO
 USE ChaoMinBeauty
 GO
-CREATE TABLE ChucVus
+CREATE TABLE ChuShops
 (
-	MaChucVu nvarchar(10) PRIMARY KEY,
-	TenChucVu nvarchar(10) NOT NULL,
-	Admin bit DEFAULT(0) NOT NULL /*Mac dinh FALSE*/ 
-)
-GO
-CREATE TABLE NhanViens
-(
-	MaNhanVien nvarchar(10) PRIMARY KEY,
-	MaChucVu nvarchar(10) NOT NULL FOREIGN KEY REFERENCES ChucVus(MaChucVu),
-	HoNhanVien nvarchar(50) NOT NULL,
-	TenNhanVien nvarchar(10) NOT NULL,
-	GioiTinh bit DEFAULT(1) NOT NULL,
-	NgaySinh date NOT NULL,
-	DiaChi nvarchar(50) NOT NULL,
-	Luong int NOT NULL,
-	AnhNhanVien nvarchar(50),
-	SDT nvarchar(10) NOT NULL,
+	Ho nvarchar(50) NOT NULL,
+	Ten nvarchar(10) NOT NULL,
+	AnhChuShop nvarchar(50),
+	Hotline nvarchar(10) PRIMARY KEY,
 	Email varchar(50) NOT NULL,
-	Password nvarchar(50) NOT NULL
+	NgaySinh date NOT NULL,
+	GioiTinh bit DEFAULT(1) NOT NULL,
+	Password nvarchar(50) NOT NULL,
+
 )
 GO
 CREATE TABLE ThanhViens
@@ -30,53 +20,88 @@ CREATE TABLE ThanhViens
 	MaThanhVien nvarchar(10) PRIMARY KEY,
 	HoThanhVien nvarchar(50) NOT NULL,
 	TenThanhVien nvarchar(10) NOT NULL,
-	NgaySinh date ,
-	GioiTinh bit DEFAULT(1), /*Mac dinh NAM*/
+	NgaySinh date NOT NULL,
+	GioiTinh bit DEFAULT(1) NOT NULL, /*Mac dinh NAM*/
+	SDT nvarchar(10) NOT NULL,
 	Email nvarchar(50) NOT NULL,
 	Password nvarchar(50) NOT NULL,
-	AnhThanhVien nvarchar(50),
-	DiaChi nvarchar (100) NOT NULL
+	AnhThanhVien nvarchar(50) NOT NULL,
+	DiaChi nvarchar (100) NOT NULL,
+
 )
 GO
 CREATE TABLE ThuongHieus
 (
 	MaThuongHieu nvarchar(10) PRIMARY KEY,
 	TenThuongHieu nvarchar(50) NOT NULL,
-	MoTa nvarchar (100),
-	AnhThuongHieu nvarchar(50),
+	MoTa nvarchar (100)NOT NULL,
+	AnhThuongHieu nvarchar(50) NOT NULL
+
 )
 CREATE TABLE DanhMucSanPhams
 (
 	MaDanhMucSanPham nvarchar(10) PRIMARY KEY,
 	TenDanhMucSanPham nvarchar(20) NOT NULL
+
 )
 GO
 CREATE TABLE SanPhams
 (
 	MaSanPham nvarchar(10) PRIMARY KEY,
 	TenSanPham nvarchar(50) NOT NULL,
-	MoTa nvarchar(100),
+	MoTa nvarchar(100) NOT NULL,
 	Gia float NOT NULL,
-	SoLuong int NOT NULL,
+	ThongTin NVARCHAR(100) NOT NULL,
+	NgayCapNhat date NOT NULL,
+	AnhSanPham NVARCHAR(50) NOT NULL,
+	SoLuongTon int NOT NULL,
 	MaThuongHieu nvarchar(10) NOT NULL FOREIGN KEY REFERENCES ThuongHieus(MaThuongHieu),
-	MaDanhMucSanPham nvarchar(10) NOT NULL FOREIGN KEY REFERENCES DanhMucSanPhams(MaDanhMucSanPham)
+	MaDanhMucSanPham nvarchar(10) NOT NULL FOREIGN KEY REFERENCES DanhMucSanPhams(MaDanhMucSanPham),
+	DaXoa bit DEFAULT(0) NOT NULL
+
 )
 GO
 CREATE TABLE DonHangs
 (
 	MaDonHang nvarchar(10) PRIMARY KEY,
 	MaThanhVien nvarchar(10) NOT NULL FOREIGN KEY REFERENCES ThanhViens(MaThanhVien),
-	NgayDatHang date NOT NULL
-	
+	NgayDatHang date NOT NULL,
+	TinhTrangGiaohang bit DEFAULT(0) NOT NULL,
+	NgayGiao date NOT NULL,
+	DaThanhToan bit DEFAULT(0) NOT NULL,
+	UuDai int NOT NULL,
+	DaHuy bit DEFAULT(0) NOT NULL,
+	DaXoa bit DEFAULT(0) NOT NULL
+
 )
 GO
 CREATE TABLE ChiTietDonHang
 (
-	MaChiTietDoHang nvarchar(10) PRIMARY KEY,
+	MaChiTietDonHang nvarchar(10) PRIMARY KEY,
 	MaDonHang nvarchar(10) NOT NULL FOREIGN KEY REFERENCES DonHangs(MaDonHang),
 	MaSanPham nvarchar(10) NOT NULL FOREIGN KEY REFERENCES SanPhams(MaSanPham),
-	MaNhanVien nvarchar(10) NOT NULL FOREIGN KEY REFERENCES NhanViens(MaNhanVien),
+	Hotline nvarchar(10) NOT NULL FOREIGN KEY REFERENCES ChuShops(SDT),
+	TenSanPham nvarchar(50) NOT NULL,
 	Gia float NOT NULL,
 	SoLuong int NOT NULL
+
+)
+GO
+CREATE TABLE PhieuNhaps
+(
+	MaPhieuNhap nvarchar(10) PRIMARY KEY,
+	MaThuongHieu nvarchar(10) NOT NULL FOREIGN KEY REFERENCES ThuongHieus(MaThuongHieu),
+	NgayNhap date NOT NULL,
+	DaXoa bit DEFAULT(0) NOT NULL
+
+)
+GO
+CREATE TABLE ChiTietPhieuNhaps
+(
+	MaChiTietPhieuNhap nvarchar(10) PRIMARY KEY,
+	MaPhieuNhap nvarchar(10) NOT NULL FOREIGN KEY REFERENCES PhieuNhaps(MaPhieuNhap),
+	MaSanPham nvarchar(10) NOT NULL FOREIGN KEY REFERENCES SanPhams(MaSanPham),
+	DonGiaNhap int NOT NULL,
+	SoLuongNhap int NOT NULL
 
 )
